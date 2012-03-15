0171df8c5902c7b899d1b94b5800519f0d68b5d4
Attila Fülöp <fulopattila122@gmail.com>
2012-03-15 16:12:56 +0200

    Updated htacces and added generic index.php file. Added app/code/Konekt according to new layout for Framework placeholder

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+/* Uncomment for development server
+error_reporting(-1);
+ini_set('display_errors', 1);
+*/
+
+require_once ('app/Konekt/Framework/Konekt.php');
+
+   try {
+      $response = Konekt::app()->getRouter()->dispatch(basename(__FILE__));
+      $response->display();
+   }
+   catch (Exception $e) {
+      Konekt::app()->getResponse()->err500($e->getMessage());
+      Konekt::app()->getResponse()->display();
+   }
